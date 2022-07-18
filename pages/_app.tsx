ce2bd89eee2789188399669bd89cf0538b880f28
Mikael <dev.kov.mtl@gmail.com>
2022-07-18 15:02:32 -0400

    App Structure

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,33 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import {useState, useEffect} from 'react'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  const [isSSR, setIsSSR] = useState(true)
+
+  useEffect(() => {
+    setIsSSR(false)
+  }, [])
+
+  if(isSSR) {
+    return null
+  }
+  
+
+  return (
+    <div>
+      Navbar
+      <div className='flex gap-6 md:gap-20'>
+        <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto">
+          Sidebar
+        </div> 
+      </div>
+      <div className="mt-4 flex flex-col gap-40 overflow-auto videos flex-1 h-[88vh]">
+        <Component {...pageProps} />
+      </div>
+    </div>
+  )
 }
 
 export default MyApp
