8b42a9e2fb3bf7c47f528d87bdf5bc3a095d42c5
Mikael <dev.kov.mtl@gmail.com>
2022-07-22 07:23:56 -0400

    Updated home page with videocard

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,37 @@
 import type { NextPage } from 'next'
+import axios from 'axios'
+import {Video} from '../types'
+import VideoCard from '../components/VideoCard'
+import NoResult from '../components/NoResult'
 
-const Home: NextPage = () => {
+interface IProps {
+  videos: Video[]
+}
+
+const Home = ({videos}: IProps) => {
   return (
-    <div className='text-3xl font-bold underline'>
-      TikTik
+    <div className='flex flex-col gap-10 videos h-full'>
+      {videos.length ? (
+        videos.map(video => (
+          <VideoCard post={video} key={video._id}/>
+        ))
+      ): (
+        <NoResult text={'No Videos'}/>
+      )}
     </div>
   )
+}
+
+export const getServerSideProps = async () => {
+  const {data} = await axios.get(`http://localhost:3000/api/post`);
+  // console.log(response.data);
+  
+  return {
+    props: {
+      videos: data
+    }
+  }
+
 }
 
 export default Home
