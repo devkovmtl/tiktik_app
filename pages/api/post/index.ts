bfa599ad3f1813899b00fbb378a8aaca5e16456c
Mikael <dev.kov.mtl@gmail.com>
2022-07-25 17:29:35 -0400

    api post video

--- pages/api/post/index.ts
+++ pages/api/post/index.ts
@@ -18,6 +18,12 @@ export default async  function handler(
         const data = await client.fetch(query);
 
         res.status(200).json(data)
+    } else if(req.method === 'POST') {
+      const doc = req.body;
+
+      client.create(doc).then(() => {
+        res.status(201).json('video created');
+      });
     }
 
 }
