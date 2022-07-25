263a5dd6729087e6456ae37f088f576e9f0ea249
Mikael <dev.kov.mtl@gmail.com>
2022-07-25 11:38:16 -0400

    upload video file

---
+++ pages/upload.tsx
@@ -0,0 +1,15 @@
+import { useState, useEffect } from 'react'
+import { useRouter } from 'next/router'
+import { FaCloudUploadAlt } from 'react-icons/fa'
+import { MdDelete } from 'react-icons/md'
+import axios from 'axios'
+
+import useAuthStore from '../store/authStore'
+import {client} from '../utils/client'
+
+const upload = () => {
+  return (
+    <div>upload</div>
+  )
+}
+export default upload
