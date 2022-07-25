26c75801352a053f44da762b71e00134eb2c260a
Mikael <dev.kov.mtl@gmail.com>
2022-07-25 17:30:00 -0400

    Upload video component

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -7,18 +7,30 @@ import { SanityAssetDocument } from "@sanity/client";
 
 import useAuthStore from "../store/authStore";
 import { client } from "../utils/client";
+import { topics } from "../utils/constants";
 
 const Upload = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [videoAsset, setVideoAsset] = useState<
     SanityAssetDocument | undefined
   >();
-  const [wrongFileType, setWrongFileType] = useState(false);
+
+  const [caption, setCaption] = useState("");
+  const [topic, setTopic] = useState<String>(topics[0].name);
+  const [loading, setLoading] = useState<Boolean>(false);
+  const [savingPost, setSavingPost] = useState<Boolean>(false);
+
+  const [wrongFileType, setWrongFileType] = useState<Boolean>(false);
+
+  const userProfile: any = useAuthStore((state) => state.userProfile);
+  const router = useRouter();
 
   const uploadVideo = async (e: any) => {
     e.preventDefault();
     const selectedFile = e.target.files[0];
+
     const fileTypes = ["video/mp4", "video/webm", "video/ogg"];
+
     if (fileTypes.includes(selectedFile.type)) {
       client.assets
         .upload("file", selectedFile, {
@@ -35,9 +47,44 @@ const Upload = () => {
     }
   };
 
+  const handleDiscard = () => {
+    setSavingPost(false);
+    setVideoAsset(undefined);
+    setCaption("");
+    setTopic("");
+  };
+
+  const handlePost = async () => {
+    if (caption && videoAsset?._id && topic) {
+      setSavingPost(true);
+
+      const doc = {
+        _type: "post",
+        caption,
+        video: {
+          _type: "file",
+          asset: {
+            _type: "reference",
+            _ref: videoAsset?._id,
+          },
+        },
+        userId: userProfile?._id,
+        postedBy: {
+          _type: "postedBy",
+          _ref: userProfile?._id,
+        },
+        topic,
+      };
+
+      await axios.post(`${process.env.BASE_URL}/api/post`, doc);
+
+      router.push("/");
+    }
+  };
+
   return (
-    <div className="flex w-full h-full">
-      <div className="bg-white rounded-lg">
+    <div className="flex w-full h-full absolute left-0 top-[60px] mb-10 pt-10 lg:pt-20 bg-[#F8F8F8] justify-center">
+      <div className="bg-white rounded-lg xl:h-[80vh] w-[80%] flex gap-6 flex-wrap justify-between items-center pt-14 pt-6">
         <div>
           <div>
             <p className="text-2xl font-bold">Upload Video</p>
@@ -51,7 +98,14 @@ const Upload = () => {
             ) : (
               <div>
                 {videoAsset ? (
-                  <div></div>
+                  <div>
+                    <video
+                      src={videoAsset?.url}
+                      loop
+                      controls
+                      className="rounded-xl h-[450px] mt-16 bg-black"
+                    ></video>
+                  </div>
                 ) : (
                   <div>
                     <label className="cursor-pointer">
@@ -85,6 +139,55 @@ const Upload = () => {
                 )}
               </div>
             )}
+            {wrongFileType && (
+              <p className="text-center text-xl text-red-400 font-semibold mt-4 w-[250px]">
+                Please select a video file
+              </p>
+            )}
+          </div>
+          <div className="flex flex-col gap-3 pb-10">
+            <label className="text-md font-medium ">Caption</label>
+            <input
+              type="text"
+              value={caption}
+              onChange={(e) => setCaption(e.target.value)}
+              className="rounded lg:after:w-650 outline-none text-md border-2 border-gray-200 p-2"
+            />
+            <label className="text-md font-medium ">Choose a topic</label>
+
+            <select
+              onChange={(e) => {
+                setTopic(e.target.value);
+              }}
+              className="outline-none lg:w-650 border-2 border-gray-200 text-md capitalize lg:p-4 p-2 rounded cursor-pointer"
+            >
+              {topics.map((item) => (
+                <option
+                  key={item.name}
+                  className=" outline-none capitalize bg-white text-gray-700 text-md p-2 hover:bg-slate-300"
+                  value={item.name}
+                >
+                  {item.name}
+                </option>
+              ))}
+            </select>
+            <div className="flex gap-6 mt-10">
+              <button
+                onClick={handleDiscard}
+                type="button"
+                className="border-gray-300 border-2 text-md font-medium p-2 rounded w-28 lg:w-44 outline-none"
+              >
+                Discard
+              </button>
+              <button
+                disabled={videoAsset?.url ? false : true}
+                onClick={handlePost}
+                type="button"
+                className="bg-[#F51997] text-white text-md font-medium p-2 rounded w-28 lg:w-44 outline-none"
+              >
+                {savingPost ? "Posting..." : "Post"}
+              </button>
+            </div>
           </div>
         </div>
       </div>
