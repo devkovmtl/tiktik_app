4dad7fd429d5214a9116fd8c23212fd3cf6411fd
Mikael <dev.kov.mtl@gmail.com>
2022-07-26 11:08:28 -0400

    Clean app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,38 +1,36 @@
-import type { AppProps } from 'next/app'
-import {useState, useEffect} from 'react'
-import { GoogleOAuthProvider } from '@react-oauth/google'
+import type { AppProps } from "next/app";
+import { useState, useEffect } from "react";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
-import '../styles/globals.css'
+import "../styles/globals.css";
 
-import Navbar from '../components/Navbar'
-import Sidebar from '../components/Sidebar'
+import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
 
 function MyApp({ Component, pageProps }: AppProps) {
-
   const [isSSR, setIsSSR] = useState(true);
 
   useEffect(() => {
     setIsSSR(false);
-  }, []);  
+  }, []);
 
   if (isSSR) return null;
-  
 
   return (
-    <GoogleOAuthProvider  clientId={`${process.env.GOOGLE_CLIENT_ID}`}>
-      <div className='xl:w-[1200px] m-auto overflow-hidden h-[100vh]'>
+    <GoogleOAuthProvider clientId={`${process.env.GOOGLE_CLIENT_ID}`}>
+      <div className="xl:w-[1200px] m-auto overflow-hidden h-[100vh]">
         <Navbar />
-        <div className='flex gap-6 md:gap-20'>
-          <div className='h-[92vh] overflow-hidden xl:hover:overflow-auto'>
+        <div className="flex gap-6 md:gap-20">
+          <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto">
             <Sidebar />
-          </div> 
-          <div className='mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1'>
+          </div>
+          <div className="mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1">
             <Component {...pageProps} />
           </div>
         </div>
       </div>
     </GoogleOAuthProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
